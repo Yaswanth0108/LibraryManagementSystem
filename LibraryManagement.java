53cf235d066fbe27e5213d7968b51adf42728ffd
pavandangeti <dangetipavan63@gmail.com>
2025-09-20 13:50:57 +0530

    Update LibraryManagement.java

--- LibraryManagement.java
+++ LibraryManagement.java
@@ -1,8 +1,9 @@
 class LibraryManagement{
 public static void main(String args[]){
  System.out.println("Hello World !!");
- System.out.println("LibraryManagement by surya");
+ System.out.println("LibraryManagement by surya bhai");
 }
 }
+
 
 
