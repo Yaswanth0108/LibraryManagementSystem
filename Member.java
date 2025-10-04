86b25c4eb4626172bf52c0661f88afa4b229634c
Yaswanth0108 <bolladayaswanth26@gmail.com>
2025-10-04 16:15:00 +0530

    Create Member.java

---
+++ Member.java
@@ -0,0 +1,17 @@
+public class Member {
+    private String rollNumber;
+    private String name;
+
+    public Member(String rollNumber, String name) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+    }
+
+    public String getRollNumber() { return rollNumber; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String toDisplayString() {
+        return "[" + rollNumber + "] " + name;
+    }
+}
